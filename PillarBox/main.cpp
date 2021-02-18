b4a35954b7a2a3f3287a611363c4d86e6c3226bd
Devharsh Trivedi <devharsh@live.in>
2021-02-17 19:35:17 -0500

    char array of 1024x1024

---
+++ PillarBox/main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  PillarBox
+//
+//  Created by Devharsh Trivedi on 2/17/21.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    /*
+    char secureAlerts [1024][1024];
+    for(int i=0; i<1024; i++) {
+        std::string alertText = "This is string number ";
+        alertText += to_string(i);
+        strncpy(secureAlerts[i], alertText.c_str(), 1023);
+        cout << secureAlerts[i] << endl;
+    }
+    */
+    
+    return 0;
+}
