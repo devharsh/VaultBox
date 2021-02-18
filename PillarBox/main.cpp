cbcdb1c3dcdcac48436389ec8595f422bcddf4b9
Devharsh Trivedi <devharsh@live.in>
2021-02-18 18:16:28 -0500

    using random number generator

--- PillarBox/main.cpp
+++ PillarBox/main.cpp
@@ -8,6 +8,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <random>
+//#include <chrono>
 
 using namespace std;
 
@@ -28,8 +30,21 @@ int main(int argc, const char * argv[]) {
         std::string alertText = "This is string number ";
         alertText += to_string(i);
         secureAlerts.push_back(alertText);
-        cout << secureAlerts[i] << endl;
+        //cout << secureAlerts[i] << endl;
     }
+    
+    //unsigned seed = static_cast<int> (chrono::system_clock::now().time_since_epoch().count());
+    //mt19937 generator(seed);
+    
+    //random_device ranDev;
+    //mt19937 mtGen(ranDev());
+    
+    mt19937 mtGen(101);
+    uniform_int_distribution<int> uniDist(0, 1023);
+    for (int i=0; i<4; i++) {
+        cout << uniDist(mtGen) << "\t";
+    }
+    cout << endl;
     
     return 0;
 }
