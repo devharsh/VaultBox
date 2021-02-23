14a0b3c8dc02b25ac6a4e8200eea7191c93e0d1a
Devharsh Trivedi <devharsh@live.in>
2021-02-23 16:24:11 -0500

    HMAC new key generation from cipher

--- PillarBox/main.cpp
+++ PillarBox/main.cpp
@@ -5,6 +5,15 @@
 //  Created by Devharsh Trivedi on 2/17/21.
 //
 
+#include <cryptopp/cryptlib.h>
+#include <cryptopp/files.h>
+#include <cryptopp/hex.h>
+#include <cryptopp/sha.h>
+#include <cryptopp/hmac.h>
+#include <cryptopp/osrng.h>
+
+using namespace CryptoPP;
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -13,83 +22,113 @@
 using namespace std;
 
 #define redundancyFactor 4
-#define bufferSize 256
+#define bufferSize 6 //256
 #define maxAlerts (redundancyFactor * bufferSize)
+#define delimiter '?'
 
-int writeVaultBox(string alert);
-void readVaultBox();
 void shuffleIndexes();
+void writeVaultBox(string alert, SecByteBlock& key);
+void readVaultBox();
 
 vector<string> vaultBox;
-vector<int> indexes;
+vector<unsigned long> indexes;
 
-int totalAlertCount = 0;
-int indexCount = 0;
+unsigned long totalAlertCount = 0;
+unsigned long indexCount = 0;
 
 int main(int argc, const char * argv[]) {
     
-    /*
-    
-    char secureAlerts [1024][1024];
-    for(int i=0; i<1024; i++) {
-        std::string alertText = "This is string number ";
-        alertText += to_string(i);
-        strncpy(secureAlerts[i], alertText.c_str(), 1023);
-        cout << secureAlerts[i] << endl;
-    }
+    /* Design:
      
-    unsigned seed = static_cast<int> (chrono::system_clock::now().time_since_epoch().count());
-    mt19937 mtGen(seed);
+     Two Apps:
+     1. SAS
+     2. SIEM
      
-    random_device ranDev;
-    mt19937 mtGen(ranDev());
+     and a configuration file?
      
-    cout << vaultBox[rand()%1024] << endl;
-    cout << vaultBox[rand()%1024] << endl;
-    cout << vaultBox[rand()%1024] << endl;
-    vaultBox[100] = "100";
-    cout << vaultBox[100] << endl;
+     SAS
+     - send buffer every 10 minutes to SIEM
+     - buffer has 256 x 4 = 1024 encrypted alerts
+     - alert = Enc (counter + alert + salt || HMAC)
      
-    */
+     SIEM
+     - if buffer not received anomaly is detected
+     - decrypt alerts, verify HMAC, discard salt
+     - identify missing counter
+     - compare alerts with same counter */
     
-    for(int i=0; i<maxAlerts; i++) {
-        indexes.push_back(i);
-        vaultBox.push_back("it is the default string");
-    }
-    
-    shuffleIndexes();
-    
-    for(int i=0; i<100; i++) {
-        std::string alertText = "This is string number ";
-        alertText += to_string(i);
-        writeVaultBox(alertText);
-    }
-    
-    readVaultBox();
-    
-    return 0;
-}
-
-int writeVaultBox(string alert) {
-    indexCount = indexCount % maxAlerts;
-    
-    for (int i=0; i<redundancyFactor; i++) {
-        vaultBox[indexes[indexCount]] = alert;
-        indexCount++;
-        totalAlertCount++;
+    try {
+        AutoSeededRandomPool prng;
+        SecByteBlock key(50);
+        prng.GenerateBlock(key, key.size());
+        // iota(indexes.begin(), indexes.end(), 0);
+        
+        for(unsigned long i=0; i<maxAlerts; i++) {
+            indexes.push_back(i);
+            vaultBox.push_back("it is the default string.. for now");
+        }
+        
+        shuffleIndexes();
+        for(int i=0; i<4; i++)
+        writeVaultBox("This is a string", key);
+        //readVaultBox();
+        
+        return 0;
     }
     
-    return totalAlertCount;
-}
-
-void readVaultBox() {
-    for(int i=0; i<maxAlerts; i++) {
-        cout << vaultBox[i] << endl;
+    catch(const CryptoPP::Exception& e) {
+        cerr << e.what() << endl;
+        exit(1);
+    }
+    
+    catch(std::exception& e) {
+        cerr << e.what() << endl;
+        exit(1);
     }
 }
 
+// SAS performs this every heartbeat
 void shuffleIndexes() {
     random_device ranDev;
     mt19937 mtGen(ranDev());
     shuffle(indexes.begin(), indexes.end(), mtGen);
+}
+
+// SAS calls this every alert
+void writeVaultBox(string alert, SecByteBlock& key) {
+    totalAlertCount++;
+    indexCount = indexCount % maxAlerts;
+    alert = to_string(totalAlertCount) + delimiter + alert;
+    
+    string encoded;
+    encoded.clear();
+    StringSource ss1(key, key.size(), true,
+                     new HexEncoder(new StringSink(encoded)));
+    
+    //cout << "key: " << encoded << endl;
+    cout << "plain text: " << alert << endl;
+    
+    HMAC< SHA256 > hmac(key, key.size());
+    StringSource ss(alert, true,
+                    new HashFilter(hmac, new StringSink(alert)));
+    
+    SecByteBlock newkey(reinterpret_cast<const byte*>(&alert[0]), alert.size());
+    key = newkey;
+    
+    encoded.clear();
+    StringSource ss2(alert, true,
+                     new HexEncoder(new StringSink(encoded)));
+    cout << "hmac: " << encoded << endl;
+    
+    for (int i=0; i<redundancyFactor; i++) {
+        vaultBox[indexes[indexCount]] = encoded;
+        indexCount++;
+    }
+}
+
+// SIEM
+void readVaultBox() {
+    for(unsigned long i=0; i<maxAlerts; i++) {
+        cout << vaultBox[i] << endl;
+    }
 }
